6c17bb7005322632d5494b772b40b169babe9493
Parth Shiyani <parthshiyani143@gmail.com>
2022-09-29 17:00:53 +0530

    firat commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:digital_clock/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
